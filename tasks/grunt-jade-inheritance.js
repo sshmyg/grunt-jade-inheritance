2b4b96d464c700538376d232acd628fe7781fe23
Adam Argyle <atom@argyleink.com>
2015-01-28 21:23:34 -0800

    support for grunt tasks in a separate folder

--- tasks/grunt-jade-inheritance.js
+++ tasks/grunt-jade-inheritance.js
@@ -3,9 +3,27 @@
 var JadeInheritance = require('jade-inheritance');
 
 module.exports = function(grunt) {
-	var config = grunt.config('jade.compile.files')[0];
+	var config = grunt.config('jade.compile.files');
 
-	if (!config) throw ('Require jade task and propper task architecture');
+	// if using single file setup
+	if (config && config.length) {
+		config = config[0];
+	}
+	// if using separate tasks folder
+	else {
+		// use load grunt configs to get the jade task config info
+		var jadeConfig = require("load-grunt-configs")(grunt, {
+			config: {
+				// we try to support for coffee or js tasks
+				src: "tasks/jade.*"
+			}
+		});
+
+		// grab first array of files, like original plan was to do
+		config = jadeConfig.jade.compile.files[0];
+
+		if (!config) throw ('Require jade task and propper task architecture');
+	}
 
 	var baseDir = config.cwd,
 		src = config.src,
